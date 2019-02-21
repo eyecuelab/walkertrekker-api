2d27bdede41ddc1a926d65c2f2ca89abd397df1a
Joe Friesen <friesen.josephc@gmail.com>
2019-02-21 15:57:29 -0800

    Tweak connectToPlayer/Campaign event listeners

--- app/socket/index.js
+++ app/socket/index.js
@@ -9,46 +9,48 @@ const registerEventListenersOnConnect = (socket) => {
   socket.on('disconnect', () => console.log(`client disconnected, socket.id = ${socket.id}`))
 
   socket.on('connectToPlayer', async (playerId) => {
+    console.log(`received connectToPlayer event, playernId ${playerId}`)
     let player = await Player.findOne({ where: { id: playerId } })
     if (player) {
       socket.join(playerId)
-      socket.emit('log', `connected to updates to player ${playerId}`)
+      socket.emit('log', `msg from server: connected to updates to player ${playerId}`)
     } else {
-      socket.emit('log', `no player found with the specified playerId`)
+      socket.emit('log', `msg from server: no player found with the specified playerId`)
     }
   })
 
   socket.on('connectToCampaign', async (campaignId) => {
+    console.log(`received connectToCampaign event, campaignId ${campaignId}`)
     let campaign = await Campaign.findOne({ where: { id: campaignId } })
     if (campaign) {
       socket.join(campaignId)
-      socket.emit('log', `connected to updates to campaign ${campaignId}`)
+      socket.emit('log', `msg from server: connected to updates to campaign ${campaignId}`)
     } else {
-      socket.emit('log', `no campaign found with the specified campaignId`)
+      socket.emit('log', `msg from server: no campaign found with the specified campaignId`)
     }
   })
 
   // call-and-response actions, mostly for demo/learning purposes
-  socket.on('initPlayerInfo', async (playerId) => {
-    socket.join(playerId)
-    console.log(`findPlayerInfo event received, fetching data for player ${playerId}`)
-    let player = await Player.findOne({ where: { id: playerId } })
-    let response = await player.toJson()
-    console.log(`Fetching data finished, result:`)
-    console.log(response)
-    console.log(`emitting playerInfoFound event`)
-    socket.emit('sendPlayerInfo', response)
-  })
-  socket.on('initCampaignInfo', async (campaignId) => {
-    socket.join(campaignId)
-    console.log(`findCampaignInfo event received, fetching data for campaign ${campaignId}`)
-    let campaign = await Campaign.findOne({ where: { id: campaignId } })
-    let response = await campaign.toJson()
-    console.log(`fetching data finished, result:`)
-    console.log(response)
-    console.log(`emitting campaignInfoFound event`)
-    socket.emit('sendCampaignInfo', response)
-  })
+  // socket.on('initPlayerInfo', async (playerId) => {
+  //   socket.join(playerId)
+  //   console.log(`findPlayerInfo event received, fetching data for player ${playerId}`)
+  //   let player = await Player.findOne({ where: { id: playerId } })
+  //   let response = await player.toJson()
+  //   console.log(`Fetching data finished, result:`)
+  //   console.log(response)
+  //   console.log(`emitting playerInfoFound event`)
+  //   socket.emit('sendPlayerInfo', response)
+  // })
+  // socket.on('initCampaignInfo', async (campaignId) => {
+  //   socket.join(campaignId)
+  //   console.log(`findCampaignInfo event received, fetching data for campaign ${campaignId}`)
+  //   let campaign = await Campaign.findOne({ where: { id: campaignId } })
+  //   let response = await campaign.toJson()
+  //   console.log(`fetching data finished, result:`)
+  //   console.log(response)
+  //   console.log(`emitting campaignInfoFound event`)
+  //   socket.emit('sendCampaignInfo', response)
+  // })
 }
 
 module.exports = {
