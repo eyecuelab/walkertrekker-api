d47ad016dd82ec047f6a238f0c9235f30c139d77
Joe Friesen <friesen.josephc@gmail.com>
2019-03-04 19:09:16 -0800

    Comment out dotenv

--- app/sched/endOfDayUpdate.js
+++ app/sched/endOfDayUpdate.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+// require('dotenv').config()
 
 const { Map } = require('immutable')
 const { getActiveCampaignsAtLocalTime, getAllActiveCampaigns, } = require('./util/getCampaigns')
--- app/sched/util/getCampaigns.js
+++ app/sched/util/getCampaigns.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+// require('dotenv').config()
 
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize(process.env.DATABASE_URL)
