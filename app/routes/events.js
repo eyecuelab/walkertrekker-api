7d4f6ce0f361dac87ceb5326a208b56c3a9086a9
Kimberly McConnell <kmcconnell393@gmail.com>
2019-07-05 12:22:15 -0700

    make randomevent script correctly create db event row, create event PATCH endpoint

--- app/routes/events.js
+++ app/routes/events.js
@@ -49,8 +49,20 @@ function eventsRouter (app) {
     })
   })
 
-
-
+  app.patch('/api/events/:eventId', appKeyCheck, fetchEvent, async function(req, res) {
+    co(async function() {
+      let event = req.event
+      console.log('now updating event')
+      await event.update(req.body.eventUpdate)
+      console.log("updated", event)
+      let json = await event.toJson();
+      console.log("json", json)
+      console.log("json", res.json(json))
+      return res.json(json)
+    }).catch((error) => {
+      return res.json({ error: "Error updating Event" })
+    })
+  })
 
 
 }
--- app/sched/randomEvent.js
+++ app/sched/randomEvent.js
@@ -12,6 +12,7 @@ const Event = sequelize.import('../models/event');
 const { getActiveCampaignsAtLocalTime, getAllActiveCampaigns, } = require('../util/getCampaigns')
 const { sendNotifications } = require('../util/notifications')
 
+
 async function randomEvent() {
 
   const campaigns = process.env.NODE_ENV == 'production'
@@ -21,14 +22,14 @@ async function randomEvent() {
   
   console.log('EVENT')
   console.log('==================')
-  console.log('EVENT')
 
   for (let campaign of campaigns) {
 
     // get players to send notifications to
     let players = await campaign.getPlayers();
     let eventType = null;
-    let eventUUID = null;
+    let eventObject = {};
+
     campaign.length === '15' ? storyFreq = 3 : campaign.length === '30' ? storyFreq = 6 : storyFreq = 12;
 
     //Story events will start on the first full day of the campaign
@@ -41,8 +42,9 @@ async function randomEvent() {
     const evtId = possibleEvents[Math.floor(Math.random() * possibleEvents.length)];
     console.log("EVENT TO INSUE", evtId)
 
-    createNewEvent = (req, res) => {
-      co(async function() {
+    
+    createNewEvent = async () => {
+      try {
         console.log('now building event')
         const newEvent = await Event.create({
           id: uuid.v4(),
@@ -51,75 +53,73 @@ async function randomEvent() {
           active: true,
           campaignId: campaign.id,
         })
-        let json = newEvent.toJson();
-        return res.json(json)
-      }).then(function (result) {
-        console.log(typeof result);
-        console.log("========== Result",  result.dataValues);
-        return result.dataValues
-      }).catch((error) => {
-        return res.json({ error: "Error creating new Event" })
-      })
-    }
-    createNewEvent();
-
-    let event = {
-      players: [],
-      data: {},
-    }
-
-    event.players = players;
-    event.data = {
-      eventType: eventType,
-      eventNumber: evtId,
-      eventId: eventUUID,
-    }
-  
-    console.log("=======================")
-
-    if (eventType === 'story') {
-      
-      for (let player of players) {
-
-        if (player.pushToken) {
-          const message = {
-            to: player.pushToken,
-            sound: 'default',
-            body: `You recieved a story event`,
-            data: {
-              type: 'eventStart',
-              data: event
-            }
-          }
-          messages.push(message)
-        }
+        let json = await newEvent.toJson();
+        eventObject = json;
+        return json
       }
-    } else if (eventType === 'random') {
-
-      for (let player of players) {
-
-      if (player.pushToken) {
-        const message = {
-          to: player.pushToken,
-          sound: 'default',
-          body: `You recieved a random event`,
-          data: {
-            type: 'eventStart',
-            data: event
-          }
-        }
-        messages.push(message)
+       catch(error) {
+        console.log({ error: "Error creating new Event" })
       }
     }
-  }
     
+
+    prepareMessages = async () => {
+      let event = {
+        players: [],
+        data: {},
+      }
+      event.players = players;
+      event.data = {
+        eventType: eventType,
+        eventNumber: evtId,
+        eventId: eventObject.id,
+      }
+      console.log("=======================")
+  
+      if (eventType === 'story') {
+        for (let player of players) {
+          if (player.pushToken) {
+            const message = {
+              to: player.pushToken,
+              sound: 'default',
+              body: `You recieved a story event`,
+              data: {
+                type: 'eventStart',
+                data: event
+              }
+            }
+            messages.push(message)
+          }
+        }
+      } else if (eventType === 'random') {
+        for (let player of players) {
+          if (player.pushToken) {
+            const message = {
+              to: player.pushToken,
+              sound: 'default',
+              body: `You recieved a random event`,
+              data: {
+                type: 'eventStart',
+                data: event
+              }
+            }
+            messages.push(message)
+          }
+        }
+      }
+    }
+
+    await createNewEvent()
+    await prepareMessages()
   }
+
+
   console.log('-------------')
   console.log(messages)
+  console.log("DATA IN THE FIRST MESSAGE", messages[0].data)
   console.log('-------------')
 
   await sendNotifications(messages)
-
   process.exit(0)
 }
 
