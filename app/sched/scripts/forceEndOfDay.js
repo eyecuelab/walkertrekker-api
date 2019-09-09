0dd016120240c9f1d866737bfd5d470c887c2a9a
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-09-09 10:29:02 -0700

    Change test players hardcoded in for cURL commands

--- app/sched/scripts/forceEndOfDay.js
+++ app/sched/scripts/forceEndOfDay.js
@@ -1,23 +1,25 @@
+require('dotenv').config();
 const { sendNotifications } = require('../../util/notifications')
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 const Campaign = sequelize.import('../../models/campaign')
 
 
+
 const update = { players:
-  [ { id: 'd88fc749-760d-43ee-a2e8-18614b87d9da',
-  displayName: 'Kim',
-  healthDiff: 7,
-  stepsDiff: 5444 },
-  { id: '6d567eb6-f2d5-48b4-9ad9-bb20171fa019',
-  displayName: 'Digi',
-  healthDiff: 30,
-  stepsDiff: -709 } ],
+  [ { id: 'ca7c667b-5ab8-4234-a384-31ddaedacec4',
+  displayName: 'Agent DEeznuts',
+  healthDiff: 0,
+  stepsDiff: 0 },
+  { id: '8810c4af-353f-40af-8682-8aa743f40c70',
+  displayName: 'Too Chainz',
+  healthDiff: 0,
+  stepsDiff: 0 } ],
   inventoryDiff: { foodItems: 0, weaponItems: 0, medicineItems: 0 },
 }
 
 const forceEndOfDay = async function() {
-  const campaign = await Campaign.findOne({ where: { id: "d6e61e24-9cb2-42bc-b363-5ca00fa58078" } })
+  const campaign = await Campaign.findOne({ where: { id: "23449e1f-26c2-41ef-9a08-588d3afe2fc0" } }) // .then(res => console.log("response" + res)).catch(err => console.error("error" + err));
   const players = await campaign.getPlayers()
   const campaignJson = await campaign.toJson()
   console.log('CAMPAIGN')
