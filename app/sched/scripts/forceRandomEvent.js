ac883873a99fff7096c9779044f5da9e560d4fe4
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-20 12:02:56 -0700

    update forceRandomEvent data structure, so that it doesn't break the front-end code data structure expectations

--- app/sched/scripts/forceRandomEvent.js
+++ app/sched/scripts/forceRandomEvent.js
@@ -2,16 +2,43 @@ require('dotenv').config()
 
 const { sendNotifications } = require('../../util/notifications')
 const Sequelize = require('sequelize')
+const uuid = require('node-uuid')
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 const Campaign = sequelize.import('../../models/campaign')
+const Event = sequelize.import('../../models/event');
 
 const forceRandomEvent = async function() {
-  const campaign = await Campaign.findOne({ where: { id: "3fd0b9d3-e9d1-4233-a064-48b76389870b" } })
+  campaignId = "7aa23a80-25ef-4767-ad98-79674f23b30c"
+  const campaign = await Campaign.findOne({ where: { id: campaignId } })
   const players = await campaign.getPlayers()
   const campaignJson = await campaign.toJson()
   console.log('CAMPAIGN')
   console.log(campaignJson)
   let messages = []
+
+  const newEvent = await Event.create({
+    id: uuid.v4(),
+    eventNumber: 1,
+    story: 'random',
+    active: true,
+    campaignId: campaignId,
+  })
+  let json = await newEvent.toJson();
+  let eventObject = json;
+
+  let event = {
+    data: {
+      eventId: eventObject.id,
+      id: eventObject.id,
+      eventNumber: 2,
+      eventType: 'random',
+      active: true,
+      campaignId: campaignId,
+      type: 'eventStart',
+      createdAt: eventObject.createdAt
+    }
+  }
+
   for (let player of campaignJson.players) {
     if (player.pushToken) {
       const message = {
@@ -20,6 +47,7 @@ const forceRandomEvent = async function() {
         body: `Your group has reached a crossroads. Tap to decide how to deal with this latest crisis.`,
         data: {
           type: 'eventStart',
+          data: event
         }
       }
       messages.push(message)
